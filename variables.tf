509255d1a2ee7ddf58b2622e6513e12a01687c95
iagovreal <iagovreal@gmail.com>
2025-09-17 14:11:34 -0300

    refactoring

--- variables.tf
+++ variables.tf
@@ -1,7 +1,2 @@
 variable "meu_ip_publico" {
-    type        = string
-    description = "Endereço IP público para o Security Group SSH"
-    # IMPORTANTE: Substitua pelo seu endereço IP público
-    # Para saber o seu IP público, acesse https://www.whatismyip.com/
-    default     = "200.150.136.37/32"
 }
